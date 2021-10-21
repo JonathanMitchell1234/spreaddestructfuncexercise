eaff2f3d1b1b5d279d5061952bff969d44a9c5c1
Jonathan Mitchell <jonmitchell1234@gmail.com>
2021-10-20 22:13:51 -0400

    SpreadDestructExercise

---
+++ script.js
@@ -0,0 +1,33 @@
+// 1a
+
+const jPDinos = ["Velocipators", "Tyrannosaurus Rex", "Dilophosaurus"];
+
+// 1B
+
+// function jPdinos([Velociraptor, Tyrannosaurus Rex, Dilophosaurus]){
+   // console.log(`Velociraptor`);
+    // console.log('Tyrannosaurus Rex' 'Dilophosaurus');
+
+
+// 2A
+
+
+
+const JPCharacters = {
+    alanGrant: "Sam Neill",
+    ellieSattler: "Laura Dern",
+    ianMalcom: "Jeff Goldblum"
+}
+console.log(JPCharacters);
+
+// 2b
+
+// seeCharacters = () => {
+//     return "laura dern";
+//   }
+
+//   console.log.(seeCharacters)
+
+seeCharacters = () => {
+    return "Laura Dern";
+  }
